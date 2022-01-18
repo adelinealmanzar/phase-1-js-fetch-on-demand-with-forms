022e7cf59ea43d3367fb40a069d48df399a8a690
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-18 07:57:20 -0800

    commit tree 30ef8e3a160fd69a8c51e6c1386d4a354091be75

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+    const form = document.querySelector('form');
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        let inputIDText = e.target.searchByID.value
+        console.log(inputIDText)
+
+        fetch(`http://localhost:3000/movies/${inputIDText}`)
+        .then(res => res.json())
+        .then(movies => {
+            const title = document.querySelector('#movieDetails h4')
+            const summary = document.querySelector('#movieDetails p')
+            title.innerText = movies.title
+            summary.innerText = movies.summary
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
